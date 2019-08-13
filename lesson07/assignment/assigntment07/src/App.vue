9030bf660a8dd3ce05624a49fd53f1a16ba8fdc9
Ian <yuppiecruncher@gmail.com>
2019-08-13 10:36:43 -0700

    Routing set up, nav bar installed on App.vue want to move to its own component in next commit.

---
+++ lesson07/assignment/assigntment07/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <router-link to="/" class="navbar-nav px-5" >Dan Smith Watercolors</router-link>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
+        <div id="navbarNav" class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+            <li class="nav-item pr-5">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+            <li class="nav-item pr-5">
+              <router-link to="/portfolio" class="nav-link">Gallery</router-link>
+            </li>
+            <li class="nav-item pr-5">
+              <router-link to="/" class="nav-link" >Home</router-link>
+            </li>
+            <li class="nav-item pr-5">
+              <router-link to="/checkout" class="nav-link">Purchase</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lesson07/assignment/assigntment07/src/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="landing">
+    <h1>Landing Page</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'LandingPage'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
