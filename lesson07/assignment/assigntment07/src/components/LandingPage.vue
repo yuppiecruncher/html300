15b5cf0c442c6866690ef4f716c42593339f33ab
Ian <yuppiecruncher@gmail.com>
2019-08-14 12:06:36 -0700

    Landing Page component complete. Moved content to variables.

--- lesson07/assignment/assigntment07/src/components/LandingPage.vue
+++ lesson07/assignment/assigntment07/src/components/LandingPage.vue
@@ -1,15 +1,67 @@
 <template>
-  <div class="landing">
-    <h1>Landing Page</h1>
-  </div>
-
+  <div class="jumbotron">
+        <h1 class="display-4">{{title}}</h1>
+        <router-link to="/about" class="btn btn-light btn-lg" href="about.html" role="button">Learn more</router-link>
+        <hr class="my-4">
+        <blockquote class="blockquote text-center p-3">
+          <p class="mb-0">{{quote}}</p>
+          <footer class="blockquote-footer">{{author}}<cite title="Source Title">{{source}}</cite></footer>
+        </blockquote>
+      </div>
 </template>
 
 <script>
 export default {
-  name: 'LandingPage'
+  name: 'LandingPage',
+  data () {
+    return {
+      msg: 'Landing Page',
+      title: 'Dan Smith Watercolors',
+      quote: "Self-expression is a hallmark of an artist, of art, to get something off one's chest, to sing one's song. So that element is present in all art. It is the key to even standing up and saying, 'Hey, listen to me.' Self-expression can be based on looking at the world and making observations about it.",
+      author: 'George Carlin ',
+      source: 'Psychology Today'
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
+
+/* google font api */
+@import url('https://fonts.googleapis.com/css?family=Beth+Ellen|Rock+Salt&display=swap');
+
+/* landing Page */
+.jumbotron {
+  background-image: url("../assets/watercolor.jpg");
+  background-size: cover;
+  text-align: center;
+  color: white;
+}
+
+h1 {
+  font-family: 'Rock Salt', cursive;
+  font-size: 2em;
+}
+
+blockquote {
+  background-color: rgba(74, 0, 0, 0.5);
+  border-radius: 5px;
+  font-size: 1em;
+  font-family: 'Beth Ellen', cursive;
+}
+  /* media queries for font sizes on different viewport widths. */
+@include media-query(tab) {
+  h1 {
+    font-size: 5em;
+  }
+  blockquote {
+    font-size: 3em;
+  }
+}
+@include media-query(xl) {
+  h1 {
+    font-size: 10em;
+  }
+}
+
 </style>
--- lesson07/assignment/assigntment07/src/components/NavBar.vue
+++ lesson07/assignment/assigntment07/src/components/NavBar.vue
@@ -21,6 +21,7 @@
         </div>
       </div>
     </nav>
+
 </template>
 
 <script>
