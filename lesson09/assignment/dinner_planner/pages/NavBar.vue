b5a5682ab37ce58827e8301eeca5f29821cc0884
Ian Smith <yuppiecruncher@gmail.com>
2019-09-02 16:05:51 -0700

    Updated pathing from router-link to nuxt-link in NavBar.vue

--- lesson09/assignment/dinner_planner/pages/NavBar.vue
+++ lesson09/assignment/dinner_planner/pages/NavBar.vue
@@ -1,21 +1,21 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <router-link to="/" class="navbar-nav px-5" >Dan Smith Watercolors</router-link>
+        <nuxt-link to="/" class="navbar-nav px-5" >Dan Smith Watercolors</nuxt-link>
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
         <div id="navbarNav" class="collapse navbar-collapse">
           <ul class="navbar-nav">
             <li class="nav-item pr-5">
-              <router-link to="/about" class="nav-link">About</router-link>
+              <nuxt-link to="/about" class="nav-link">About</nuxt-link>
             </li>
             <li class="nav-item pr-5">
-              <router-link to="/portfolio" class="nav-link">Gallery</router-link>
+              <nuxt-link to="/portfolio" class="nav-link">Gallery</nuxt-link>
             </li>
             <li class="nav-item pr-5">
-              <router-link to="/checkout" class="nav-link">Purchase</router-link>
+              <nuxt-link to="/checkout" class="nav-link">Purchase</nuxt-link>
             </li>
             <li class="nav-item pr-5">
-              <router-link to="/" class="nav-link" >Home</router-link>
+              <nuxt-link to="/" class="nav-link" >Home</nuxt-link>
             </li>
           </ul>
         </div>
